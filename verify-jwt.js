911260b26eb745396ea8558f31b6e1c3e577e9e0
Travis Johnson <tjohnson@appnexus.com>
2016-10-31 12:36:16 -0400

    adding check for jwt

--- verify-jwt.js
+++ verify-jwt.js
@@ -4,13 +4,19 @@ var fs = require('fs');
 
 var key_name = process.argv[2];
 if (!key_name) {
-	console.error("You must provide the public key name as the only argument to this script");
+	console.error("You must provide the public key name as the first argument to this script");
+	process.exit(1);
+}
+
+var token = process.argv[3];
+if (!token) {
+	console.error("You must provide the jwt as the second argument to this script");
 	process.exit(1);
 }
 
 var cert = fs.readFileSync(key_name)
 var epoch_time = Math.floor((new Date).getTime());
-var decoded = jwt.verify(process.argv[3], cert, { algorithms: ['RS256', 'HS256'] }, function(err, decoded) {
+var decoded = jwt.verify(token, cert, { algorithms: ['RS256', 'HS256'] }, function(err, decoded) {
 	if (err) {
 		console.log(err);
 	} else {
